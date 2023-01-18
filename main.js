f9eb2c79ad8e3e12af17e7139ec6d7cca68244f7
LeonardoContino <leoconti2000@hotmail.it>
2023-01-18 15:46:31 +0100

    aggiunto 10 email e provato a fare il loading

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-console.log("js ok");
+// console.log("js ok");
+
+
+/*
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+Super Bonus
+Riusciamo a gestire e mostrare un loader mentre le mail non sono ancora tutte pronte? :sorriso:
+
+*/
+const app = Vue.createApp({
+    data(){
+        return{
+            emails: [],
+            isLoading: false
+        }
+    },
+    methods:{
+        getRandomEmail() {
+        this.isLoading = true;
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(response =>{
+        this.emails.push(response.data.response) ;
+        })
+        .then(()=>{
+            this.isLoading = false;
+        })
+    
+        }
+    },
+    mounted(){
+        for(let i = 0; i < 10; i++){
+            this.getRandomEmail()
+        }
+    
+    }
+})
+
+
+
+
+app.mount('#root')
